76a5f94a56414cb31f4379f8bf57412a63f91c17
Moses Alcantara <mosesalcantara123@gmail.com>
2024-07-04 14:05:06 +0800

    fix email links

--- resources/views/mail/admin_viewing.blade.php
+++ resources/views/mail/admin_viewing.blade.php
@@ -3,11 +3,11 @@
 <p>{{ $mail_data['name'] }} has requested a viewing of Unit {{ $mail_data['unit_no'] }} at {{ $mail_data['property'] }} on {{ $mail_data['date'] }} at {{ $mail_data['time'] }}.</p> <br>
 
 <div style="display:flex;">
-@component('mail::button', ['url' => "http://127.0.0.1:8000/appointment/approve/" . $mail_data['id'], 'color' => 'green'])
+@component('mail::button', ['url' => "https://alveolandcorp.online/appointment/approve/" . $mail_data['id'], 'color' => 'green'])
 Accept
 @endcomponent
 
-@component('mail::button', ['url' => "http://127.0.0.1:8000/appointment/decline/" . $mail_data['id'], 'color' => 'red'])
+@component('mail::button', ['url' => "https://alveolandcorp.online/appointment/decline/" . $mail_data['id'], 'color' => 'red'])
 Decline
 @endcomponent
 </div>
